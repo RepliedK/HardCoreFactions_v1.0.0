d9c72897adf3eebe09c590cc77d98a15af9a924d
RepliedK <repliedk@gmail.com>
2024-02-21 22:19:29 -0600

    Update DataProvider.php

--- src/hcf/provider/DataProvider.php
+++ src/hcf/provider/DataProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace hcf\provider;
 
-use hcf\database\DataSaver;
 use hcf\HCFLoader;
 use hcf\utils\logic\serialize\Serialize;
 use hcf\module\package\PackageManager;
